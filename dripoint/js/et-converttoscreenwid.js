8373e4fd144b8039481d3e68aedc510c59e1e970
jaysturm <jaysturm@gmail.com>
2014-05-08 15:07:01 -0600

    updating et-converttoscreenwid file to comply with current system.  also added a way to remove html elements before they are saved into storage.  this is for local conversions

--- dripoint/js/et-converttoscreenwid.js
+++ dripoint/js/et-converttoscreenwid.js
@@ -1,7 +1,6 @@
 // copyright (c) 2014 DRI
 $(document).ready(function () {
-    var fileName = window.location.href.split('/').pop(),
-        widName = fileName.substr(0, fileName.indexOf('.')),
+    var widName = widAppHelper.getUrlParam('wid'),
         params = {};
 
     if (typeof widforview !== 'undefined') { params.widforview = widforview; }
@@ -13,12 +12,17 @@ $(document).ready(function () {
     // append any existing style blocks to body before continuing
     $('style').each(function () { $('body').append(this.outerHTML); });
 
+    // delete any 'et-delete' classes
+    // useful for removing the link to this file before saving to storage
+    $('.et-delete').remove();
+
     // convert linked html page to a screenwid
     htmlToScreenwid(widName, $('body').html(), params, function() {
         $('body').html('<div class="container" style="margin-top:30px;text-align:center;">'
-                     + '<div class="row well col-md-8 col-md-offset-2"><h4>This page has been saved as the '
-                     + widName + ' screenwid.<br /> ' + 'You will be redirected to <a href="http://dripoint.com?wid='
-                     + widName + '">http://dripoint.com?wid = ' + widName + '</a></h4></div></div>');
-        setTimeout(function() { window.location = 'http://dripoint.com?wid=' + widName; },3500);
+            + '<div class="row well col-md-8 col-md-offset-2"><h4>This page has been saved as the '
+            + widName + ' screenwid in the current angular data model as well as in localStorage.<br /> '
+            + 'You will be redirected to <a href="http://dripoint.com?wid='
+            + widName + '">http://dripoint.com?wid = ' + widName + '</h4></div></div>');
+        setTimeout(function() { window.location = 'http://test2.dripoint.com?wid=' + widName; },3500);
     });
 });
