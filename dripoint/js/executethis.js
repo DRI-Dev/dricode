6c280d707e5e31bc0f8be950fbd5938511a18e05
Jason <jaysturm@gmail.com>
2014-06-19 16:45:03 -0600

    changing allowed color leve to 10

--- dripoint/js/executethis.js
+++ dripoint/js/executethis.js
@@ -418,7 +418,7 @@
 
         //proxyprinttodiv('execute level type ', String(executionpreferences.command.environment.run.executelevel) + ' ' + type, 11);
 
-        if (executionpreferences.command.environment.run.executelevel > 10 || color > 500) {
+        if (executionpreferences.command.environment.run.executelevel > 10 || color > 10) {
             callback({"errorname":"level too high"}, null);
         } 
         else 
