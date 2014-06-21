5e48849dc8839f9b69f384fcd30740dc476742ca
Jason <jaysturm@gmail.com>
2014-06-20 19:29:42 -0600

    correcting conditional statement in et-utils, removed undefined variable

--- dripoint/js/et-utils.js
+++ dripoint/js/et-utils.js
@@ -724,7 +724,8 @@ exports.getwid = getwid = function getwid(inputWidgetObject, callback) {
             }
         } else if (datastore === 'mongo') {
             mget(inputWidgetObject, command, function (err, output) {
-                if (!keepaddthis && output) { // i.e. remove add this
+//                if (!keepaddthis && output) { // i.e. remove add this
+                if (output) {  // remove add this
                     if (output.hasOwnProperty("addthis")) {
                         var _add_this = output["addthis"];
                         delete output["addthis"];
