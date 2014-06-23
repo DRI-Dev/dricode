8658b19e895abfa277d187d01fc8e7d396ec9d82
Jason <jaysturm@gmail.com>
2014-06-23 15:30:42 -0600

    adding deletewid test

--- dripoint/js/et-test.js
+++ dripoint/js/et-test.js
@@ -9004,6 +9004,14 @@ exports.ettestgetwidserver = ettestgetwidserver = function ettestgetwidserver(pa
     });
 };
 
+exports.ettestdeletewid = ettestdeletewid = function ettestdeletewid(params, callback) {
+    var etEnvironment = new drienvironment(params.command.environment);
+    etEnvironment.execute({executethis:'deletewid',wid:'testdata123'}, function (err, result) {
+        proxyprinttodiv('getwid results', result, 99);
+        callback(err, result);
+    });
+};
+
 exports.ettestaddwidmaster = ettestaddwidmaster = function ettestaddwidmaster(params, callback) {
     var etEnvironment = new drienvironment(params.command.environment);
     etEnvironment.execute({executethis:'addwidmaster',wid:'mastertestdata123',key1:'value1',key2:'value2'}, function (err, result) {
