2a3128d27f612c42c4645dcd1371ba70ea573763
Jason <jaysturm@gmail.com>
2014-06-26 15:04:22 -0600

    changing same code as last commit but still leaving it commented

--- dripoint/js/et-query.js
+++ dripoint/js/et-query.js
@@ -828,14 +828,17 @@ exports.querywid = querywid = function querywid(parameters, callback) { // can c
 
                             function (cb1) {
                                 proxyprinttodiv('querywid finalformatlist widrecord ', widrecord, 17);
-//                                var widrecordFixed = {};
-//                                widrecordFixed['data'] = Array.isArray(widrecord) ? widrecord[0] : widrecord;
-//                                widrecordFixed['metadata'] = Array.isArray(widrecord) ? widrecord[0]['metadata'] : widrecord['metadata'];
-//                                widrecordFixed['wid'] = Array.isArray(widrecord) ? widrecord[0]['wid'] : widrecord['wid'];
-//                                extrarecord[environmentdb] = extraparameters[wid];
-//                                Array.isArray(widrecord) ? delete widrecord[0]['wid'] : delete widrecord['wid'];
-//                                Array.isArray(widrecord) ? delete widrecord[0]['metadata'] : delete widrecord['metadata'];
-//                                widrecord = widrecordFixed;
+
+//                                if (Array.isArray(widrecord)) {
+//                                    var widrecordFixed = {};
+//                                    widrecordFixed['data'] = widrecord[0];
+//                                    widrecordFixed['metadata'] = widrecord[0]['metadata'];
+//                                    widrecordFixed['wid'] = widrecord[0]['wid'];
+//                                    extrarecord[environmentdb] = extraparameters[wid];
+//                                    delete widrecord[0]['wid'];
+//                                    delete widrecord[0]['metadata'];
+//                                    widrecord = widrecordFixed;
+//                                }
 
                                 proxyprinttodiv('querywid finalformatlist widrecord', convertfromdriformat(widrecord), 17);
                                 proxyprinttodiv('querywid finalformatlist extraparameters[wid]', extrarecord, 17);
