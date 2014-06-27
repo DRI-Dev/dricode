b446a71815a28ec33d9d6aabd01e2440682dfaab
Jason <jaysturm@gmail.com>
2014-06-26 21:25:29 -0600

    adding image buffering logic to images.js and updating ngWidApp.js

--- nodeapi/routes/images.js
+++ nodeapi/routes/images.js
@@ -7,14 +7,15 @@ exports.saveBase64ToServer = saveBase64ToServer = function saveBase64ToServer(re
         path = parameters.path,
         startIndex = (path.indexOf('\\') >= 0 ? path.lastIndexOf('\\') : path.lastIndexOf('/')),
         filename = path.substring(startIndex + 1),
-        base64image = parameters.imagesrc.replace(/^data:image\/png;base64,/,"");
+//        base64image = parameters.imagesrc.replace(/^data:image\/png;base64,/,""),
+        imageBuffer = decodeBase64Image(parameters.imagesrc);
 
     console.log('** Image service is saving ' + filename + ' from base64 string **');
 
-    fs.writeFile("C:\\servers\\master\\dripoint\\images\\" + filename, base64image, 'base64', function(err) {
+    fs.writeFile("C:\\servers\\master\\dripoint\\images\\" + filename, imageBuffer.data, 'base64', function(err) {
         if (err) {
             console.log('** Image service error saving ' + filename + ' =>');
-            consoel.log(err);
+            console.log(err);
             res.send('failure');
         } else {
             console.log('** Image service successfully saved dripoint/images/' + filename + ' **');
@@ -24,3 +25,17 @@ exports.saveBase64ToServer = saveBase64ToServer = function saveBase64ToServer(re
         res.end();
     });
 };
+
+function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+    if (matches.length !== 3) {
+        return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+}
