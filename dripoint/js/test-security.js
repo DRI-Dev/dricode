3a9a50c175f2382e87fb0c7296783e2c74e129a6
rogerjc <roger@dri.com>
2014-07-11 04:40:58 -0400

    sec tst

--- dripoint/js/test-security.js
+++ dripoint/js/test-security.js
@@ -184,3 +184,140 @@ widtests.setests_metadataaddtogroup1.category = "daily";
 widtests.setests_metadataaddtogroup1.subcategory = "push";
 widtests.setests_metadataaddtogroup1.js = exports.setests_metadataaddtogroup1;
 widtests.setests_metadataaddtogroup1.description = "this does a test";
+
+
+
+// Creates a wid and adds that wid to a group. Assigns the "edit" actiongroup (permission) to the "creator" usergroup.
+// logverify needs to verify that the wid was added to the group... not correct right now
+exports.setests_metadataaddgrouppermissions1 = 
+setests_metadataaddgrouppermissions1 = 
+widtests.setests_metadataaddgrouppermissions1 = 
+function setests_metadataaddgrouppermissions1 (executeobject, callback) {
+
+      if (!executeobject.command) {
+      executeobject.command={};
+      executeobject.command.environment={};
+      executeobject.command.environment.run={};
+	  };
+		
+      executeobject.command.xrun=[{
+									"executethis": "updatewid",
+									"wid":"mycolorwid1",
+									"color":"green",
+									"metadata": {
+										"security": {
+											"group": {
+												"colorwids":"colorwids"
+													},
+											"permissions": [{
+												"usergroup": "creator",
+												"actiongroup": "edit",
+												"level": 99
+													}]
+												}
+										}
+									}
+								];
+		
+		var expectedresult = {
+								"wid":"mycolorwid1",
+								"data": {
+											"color":"green"
+										},
+								"metadata": {
+												"expirationdate":{"exception":["created","changed","unchanged","updated"]},
+												"date":{"exception":["created","changed","unchanged","updated"]}
+											}
+							}
+		
+      var etEnvironment = new drienvironment(executeobject.command.environment)
+      etEnvironment.execute(executeobject, function (error_obj, result_obj) 
+      {
+                                
+            proxyprinttodiv('expected error', null, 99);
+            proxyprinttodiv('actual error', error_obj, 99);
+            proxyprinttodiv('expected result', expectedresult, 99);
+            proxyprinttodiv('actual result', result_obj, 99);
+            composite_obj=logverify("setests_metadataaddgrouppermissions1", result_obj,expectedresult);
+            callback(null, composite_obj)
+      } 
+    );
+};
+widtests.setests_metadataaddgrouppermissions1.category = "daily";
+widtests.setests_metadataaddgrouppermissions1.subcategory = "push";
+widtests.setests_metadataaddgrouppermissions1.js = exports.setests_metadataaddgrouppermissions1;
+widtests.setests_metadataaddgrouppermissions1.description = "this does a test";
+
+
+// Creates a wid and adds that wid to a group. Assigns the edit / delete / execute / copy actions to creator usergroup
+// logverify needs to verify that the wid was added to the group... not correct right now
+exports.setests_metadataaddgrouppermissions2 = 
+setests_metadataaddgrouppermissions2 = 
+widtests.setests_metadataaddgrouppermissions2 = 
+function setests_metadataaddgrouppermissions2 (executeobject, callback) {
+
+      if (!executeobject.command) {
+      executeobject.command={};
+      executeobject.command.environment={};
+      executeobject.command.environment.run={};
+	  };
+		
+      executeobject.command.xrun=[{
+									"executethis": "updatewid",
+									"wid":"mycolorwid1",
+									"color":"green",
+									"metadata": {
+										"security": {
+											"group": {
+												"colorwids":"colorwids"
+													},
+											"permissions": [{
+												"usergroup": "creator",
+												"actiongroup": "edit",
+												"level": 99
+													}, {
+												"usergroup": "creator",
+												"actiongroup": "delete",
+												"level": 99
+													}, {
+												"usergroup": "creator",
+												"actiongroup": "execute",
+												"level": 99
+													}, {
+												"usergroup": "creator",
+												"actiongroup": "copy",
+												"level": 99
+													}]
+											}
+										}
+									}
+								];
+		
+		var expectedresult = {
+								"wid":"mycolorwid1",
+								"data": {
+											"color":"green"
+										},
+								"metadata": {
+												"expirationdate":{"exception":["created","changed","unchanged","updated"]},
+												"date":{"exception":["created","changed","unchanged","updated"]}
+											}
+							}
+		
+      var etEnvironment = new drienvironment(executeobject.command.environment)
+      etEnvironment.execute(executeobject, function (error_obj, result_obj) 
+      {
+                                
+            proxyprinttodiv('expected error', null, 99);
+            proxyprinttodiv('actual error', error_obj, 99);
+            proxyprinttodiv('expected result', expectedresult, 99);
+            proxyprinttodiv('actual result', result_obj, 99);
+            composite_obj=logverify("setests_metadataaddgrouppermissions2", result_obj,expectedresult);
+            callback(null, composite_obj)
+      } 
+    );
+};
+widtests.setests_metadataaddgrouppermissions2.category = "daily";
+widtests.setests_metadataaddgrouppermissions2.subcategory = "push";
+widtests.setests_metadataaddgrouppermissions2.js = exports.setests_metadataaddgrouppermissions2;
+widtests.setests_metadataaddgrouppermissions2.description = "this does a test";
