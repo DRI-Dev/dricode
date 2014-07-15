dfa85ca8bf5aef480d451b33ce382b423c90ab2a
DubLo7 <russryba@gmail.com>
2014-07-15 15:03:44 -0400

    Adding GET processing to web.js

--- nodeapi/web.js
+++ nodeapi/web.js
@@ -57,7 +57,7 @@ app.configure('development', function() {
 //// *********************** Route Mapping for Application follows   ***********************
 //app.put('/executethis', server.putpostgetrunExecutethis);
 //app.post('/executethis', server.putpostgetrunExecutethis);
-//app.get('/executethis', server.putpostgetrunExecutethis);
+app.get('/executethis', server.putpostgetrunExecutethis);
 app.put('/executethis', server.putrunExecutethis);
 app.put('/filetowid', convert.convertFileToWid);
 app.put('/base64toserver', imageService.saveBase64ToServer);
