7e1305a2e39ac4c4ac950a63b5818d2bf188b4c8
DubLo7 <russryba@gmail.com>
2014-07-31 13:47:10 -0400

    Callback fails... trying to fix it.

--- nodeapi/config-server.js
+++ nodeapi/config-server.js
@@ -1097,7 +1097,9 @@ exports.publishtestdelay = publishtestdelay = function publishtestdelay(paramete
                 'body': 'publishtest - status: ' + passfail + ', user: ' + pusher_name + 
                     ", repo name: " + repo_name + ', ref: ' + ref  
                 }, 
-                callback(err, result);
+                function (err, result) {
+                    callback(err, result);
+                }
             );
         });
         //sendPostCall({"post_data":parameters}, function(err, result) {
