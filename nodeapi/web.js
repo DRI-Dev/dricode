efeaad803bd59ca1f083cca09bdc74b9caa525a4
rogerjc <roger@dri.com>
2014-08-04 13:58:14 -0400

    major chagnes web.js

--- nodeapi/web.js
+++ nodeapi/web.js
@@ -5,16 +5,61 @@ require('./create_config.js');
 require('./config.js');
 require('./config-server.js');
 
-console.log('----X99');
 
-console.log('russ1');
-// console.log(russ1);
-console.log('russ2');
-console.log(russ2);
-console.log('russ3');
-// console.log(russ3);
-console.log('russ4');
-console.log(russ4);
+// switch between the location of core files
+var DIR_TO_CORE_JS = '../dripoint/js/';
+
+require(DIR_TO_CORE_JS + 'et-dto.js');
+require(DIR_TO_CORE_JS + 'et-security.js');
+exports.utils = require(DIR_TO_CORE_JS + 'et-utils.js');
+require(DIR_TO_CORE_JS + 'et-add.js');
+require(DIR_TO_CORE_JS + 'et-get.js');
+require(DIR_TO_CORE_JS + 'et-test.js');
+require(DIR_TO_CORE_JS + 'test-addhoc.js');
+require(DIR_TO_CORE_JS + 'test-dto.js');
+require(DIR_TO_CORE_JS + 'test-et-utils.js');
+require(DIR_TO_CORE_JS + 'test-execute.js');
+require(DIR_TO_CORE_JS + 'test-inherit.js');
+require(DIR_TO_CORE_JS + 'test-main.js');
+require(DIR_TO_CORE_JS + 'test-query.js');
+require(DIR_TO_CORE_JS + 'test-security.js');
+require(DIR_TO_CORE_JS + 'tests-up-for-review.js');
+require(DIR_TO_CORE_JS + 'et-query.js');
+exports.executethis = require(DIR_TO_CORE_JS + 'executethis.js');
+
+exports.config = require('./config-server.js');
+
+exports.configuration = configuration = config.configuration;
+
+// Server specific Routes here
+console.log('server.js -- bof');
+
+require('../config.js');
+
+var async = require('async');
+
+
+// var mongoskin = require('mongoskin'),
+//     SkinStore = require('connect-mongoskin');
+console.log('server.js -- a002');
+
+  path = require('path')
+console.log('server.js -- a003');
+    // dao = require('../dao/alterdao.js'),
+    dao = require('../dao/mongo.js')
+console.log('server.js -- a004');
+    superagent = require('superagent')
+console.log('server.js -- a005');
+    https = require('https')
+console.log('server.js -- a006');
+    querystring = require('querystring')
+console.log('server.js -- a007');
+    url = require('url')
+console.log('server.js -- a008');
+    util = require('util');
+require('../dao/mongotest.js');
+// , drifn = require('../dao/dri_functions.js')
+
 
 express = require('express')
 console.log('---44a');
@@ -26,7 +71,7 @@ console.log('---44c');
 console.log('---44d');
     request = require('request')
 console.log('---44e');
-    server = require('./routes/server')
+//    server = require('./routes/server')
 console.log('---44f');
     convert = require('./routes/convert.js')
 console.log('---44g');
@@ -88,22 +133,17 @@ console.log('server config is ' + serverconfig.SERVER_PORT);
 
 exports.window = {"configuration":config.configuration};
 
-console.log(JSON.stringify(config));
-
-
-
-sendsms({
-    'tonumber': '+12313133930',
-    'msgbody': 'This the server- I just restarted '
-}, function (err, result) {
-    //console.log('running');
-});
 
 console.log('port is ' + serverconfig.SERVER_PORT);
 app.listen(process.env.PORT || serverconfig.SERVER_PORT);
 
 
 eventdeviceready({}, function (err, res) {
+    sendsms({
+    'tonumber': '+12313133930',
+    'msgbody': 'This the server- I just restarted '
+}, function (err, result) {
+    //console.log('running');
 });
-
+});
 
