8607e70968e7099dce211ec021d5c6b45d504a89
Jason <jaysturm@gmail.com>
2014-08-06 11:56:50 -0600

    console.re changes

--- nodeapi/config-server.js
+++ nodeapi/config-server.js
@@ -94,9 +94,10 @@ global.Debug = 'true';
 global.debuglevel = 0;
 global.debugon = false;
 //global.consolere = require('console-remote-client').connect('console.re','80','dev-dri');
-global.console = console = require('console-remote-client').connect('console.re','80','dev-dri');
+var consolere = global.console = console = require('console-remote-client').connect('console.re','80','dev-dri');
 global.localStorage = exports.localStorage = {};
 
+console.re.log('remote log test');
 
 // these are internal to this file
 var needle = require('needle');
