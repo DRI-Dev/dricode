31b9e5ac70760e5f5534323e302a0b83ab90a462
Jason <jaysturm@gmail.com>
2014-08-06 10:05:42 -0600

    correcting namespace issue

--- nodeapi/config-server.js
+++ nodeapi/config-server.js
@@ -94,7 +94,7 @@ global.Debug = 'true';
 global.debuglevel = 0;
 global.debugon = false;
 global.consolere = require('console-remote-client').connect('console.re','80','dev-dri');
-global.console = exports.consolere;
+global.console = global.consolere;
 global.localStorage = exports.localStorage = {};
 
 
