eb26e0ddb3fbf02e952bc986df2090b78c34d336
Jason <jaysturm@gmail.com>
2014-08-07 16:38:14 -0600

    adding papertrail logic

--- nodeapi/web.js
+++ nodeapi/web.js
@@ -37,6 +37,18 @@ url = require('url');
 util = require('util');
 
 stringifyObject = require('stringify-object');
+winston = require('winston-papertrail').Papertrail;
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.Papertrail({
+            host: 'logs.papertrailapp.com',
+            port: '8889'
+        })
+    ]
+});
+
+logger.info('this is a papertrail log');
+
 express = require('express');
 app = express();
 http = require('http');
