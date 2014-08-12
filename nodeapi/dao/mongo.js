09ed2e50b60e0345f8c0bde7e3a196e65e738cc2
Jason <jaysturm@gmail.com>
2014-08-12 11:28:32 -0600

    changing postman wget test function

--- nodeapi/dao/mongo.js
+++ nodeapi/dao/mongo.js
@@ -178,10 +178,6 @@ exports.wget = wget = function wget(objToFind, command, callback) {
 };
 
 exports.wgettest = wgettest = function wgettest(objToFind, callback) {
-    (command && command.db) ? databaseToLookup = command.db : databaseToLookup;
-    (command && command.databasetable) ? mongoDatabaseToLookup = command.databasetable : mongoDatabaseToLookup;
-    (command && command.collection) ? schemaToLookup = command.collection : schemaToLookup;
-
     if (typeof objToFind === "string") { objToFind = JSON.parse(objToFind); }
 
     getConnection(mongoDatabaseToLookup, function(err, db) {
