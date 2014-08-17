7ffdc410ea877c2f64fa7699b6eb597d50ca2255
rogerjc <roger@dri.com>
2014-08-17 14:24:05 -0400

    adding dynamic sort

--- dripoint/js/et-utils.js
+++ dripoint/js/et-utils.js
@@ -928,6 +928,45 @@
         }
     };
 
+
+
+// http://stackoverflow.com/questions/1129216/sorting-objects-in-an-array-by-a-field-value-in-javascript
+// use: People.sort(dynamicsort("Name"));
+// use: People.sort(dynamicsortmultiple("Name", "-Surname"));
+    exports.dynamicsort = window.dynamicsort = dynamicsort = function dynamicsort(property) 
+    {
+        var sortOrder = 1;
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a,b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
+    }
+    
+    exports.dynamicsortmultiple = window.dynamicsortmultiple = dynamicsortmultiple = function dynamicsortmultiple() {
+    /*
+     * save the arguments object as it will be overwritten
+     * note that arguments object is an array-like object
+     * consisting of the names of the properties to sort by
+     */
+    var props = arguments;
+    return function (obj1, obj2) {
+        var i = 0, result = 0, numberOfProperties = props.length;
+        /* try getting a different result from 0 (equal)
+         * as long as we have extra properties to compare
+         */
+        while(result === 0 && i < numberOfProperties) {
+            result = dynamicsort(props[i])(obj1, obj2);
+            i++;
+        }
+        return result;
+    }
+}
+
+
     //
     //   this will command.dtotype inisde bigdto
     //   this will give an address for dtotype inside bigdto
