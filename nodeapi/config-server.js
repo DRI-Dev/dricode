dab3b1771050ac0e23614600d68243dc36722b78
DubLo7 <russryba@gmail.com>
2014-08-19 14:33:39 -0400

    Changed default phone number to MY phone number for testing.

--- nodeapi/config-server.js
+++ nodeapi/config-server.js
@@ -531,6 +531,7 @@ exports.publishtest = publishtest = function publishtest(parameters, callback) {
         executeobject.server = server;
         
         var default_number = '+12145644732';
+        default_number = '+12313133930';
         var pusher_numbers = {
             'rogerjs' : '+12145644732',
             'info@dri.com' : default_number,
