ccb0d7f9586c653561293e4cd9e4b8520ac9b62f
root <jaysturm@gmail.com>
2014-09-03 14:24:52 -0600

    updating convert logic

--- nodeapi/routes/convert.js
+++ nodeapi/routes/convert.js
@@ -7,8 +7,7 @@ var cheerio = require('cheerio')
 exports.convertFileToWid = convertFileToWid = function convertFileToWid(req, res) {
     var parameters = req.body,
         filename = parameters.filename,
-        filepath = parameters.path,
-        fileContents = filesys.readFileSync(filepath).toString(),
+        fileContents = parameters.contents,
         shortFilename = filename.slice(0, filename.indexOf('.')),
         extension = filename.slice(filename.indexOf('.') + 1, filename.length),
         $ = cheerio.load(fileContents),
@@ -34,49 +33,12 @@ exports.convertFileToWid = convertFileToWid = function convertFileToWid(req, res
             res.end();
         });
     } else if (extension === 'html') {
-        // find screenwid variables from fileContents
-        var wfv = fileContents.match("var widforview = .*[,,;]");
-        if (wfv && wfv.length > 0) {
-            wfv = wfv.toString().substring(wfv.toString().indexOf('=') + 3, wfv.toString().length - 2).trim();
-            newScreenwid.widforview = wfv;
-        }
-
-        var wfb = fileContents.match("var widforbase = .*[,,;]");
-        if (wfb && wfb.length > 0) {
-            wfb = wfb.toString().substring(wfb.toString().indexOf('=') + 3, wfb.toString().length - 2).trim();
-            newScreenwid.widforbase = wfb;
-        }
-
-        var wfbg = fileContents.match("var widforbackground = .*[,,;]");
-        if (wfbg && wfbg.length > 0) {
-            wfbg = wfbg.toString().substring(wfbg.toString().indexOf('=') + 3, wfbg.toString().length - 2).trim();
-            newScreenwid.widforbackground = wfbg;
-        }
-
-        var dfv = fileContents.match("var dataforview = .*[,,;]");
-        if (dfv && dfv.length > 0) {
-            dfv = dfv.toString().substring(dfv.toString().indexOf('=') + 3, dfv.toString().length - 2).trim();
-            newScreenwid.dataforview = dfv;
-        }
-
-        var lks = fileContents.match("var links = .*[,,;]");
-        if (lks && lks.length > 0) {
-            lks = lks.toString().substring(lks.toString().indexOf('=') + 2, lks.toString().length - 1).trim();
-            newScreenwid.links = lks;
-        }
-
-        var strtwd = fileContents.match("var startwid = .*[,,;]");
-        if (strtwd && strtwd.length > 0) {
-            strtwd = strtwd.toString().substring(strtwd.toString().indexOf('=') + 3, strtwd.toString().length - 2).trim();
-            newScreenwid.startwid = strtwd;
-        }
-
         processExecuteTags($, function() {
             finishConvert($, newScreenwid, res);
         });
     } else {
         console.log('** File to Wid conversion process failed for file ' + filename + ' ,\n'
-                    + '    we currently do not convert files of type .' + extension + ' **');
+            + '    we currently do not convert files of type .' + extension + ' **');
 
         res.send('failed');
         res.end();
@@ -104,7 +66,7 @@ function processExecuteTags($, callback) {
                 if (onbuild) {
                     console.log('**$$**  processing execute, onbuild true.  Execute params => ' + JSON.stringify(executeObj));
 
-                    execute(executeObj, function (err, results) {
+                    execute(executeObj, function (err, resultArray) {
                         if (err && Object.size(err) > 0) { console.log('** error found => ' + JSON.stringify(err)); }
                         else {
                             if (results.addthis) { results = removeAddThis(results); }
@@ -136,7 +98,7 @@ function finishConvert($, screenwid, res) {
 
     console.log('**>>** new screenwid execution parameters => ' + JSON.stringify(screenwid));
 
-    execute(screenwid, function(err, resultsArray) {
+    execute(screenwid, function(err, results) {
         if (err && Object.size(err) > 0) { console.log('** htmlToScreenwid addwidmaster error => ' + JSON.stringify(err) + ' **'); }
         else { console.log('** screenwid ' + screenwid.wid + ' has been created. **'); }
 
@@ -160,15 +122,6 @@ cheerio.prototype.allAttrs = function() {
     return this[0].attribs;
 };
 
-// convert a NamedNodeMap to an Object
-function NNMtoObj(namedNodeMap) {
-    var obj = {};
-    for (var i = 0; i < namedNodeMap.length; i++) {
-        obj[namedNodeMap[i].name] = namedNodeMap[i].value;
-    }
-    return obj;
-}
-
 function removeAddThis(addThisObj) {
     for (var prop in addThisObj.addthis) {
         if (addThisObj.addthis.hasOwnProperty(prop)) {
@@ -177,22 +130,4 @@ function removeAddThis(addThisObj) {
     }
     delete addThisObj['addthis'];
     return addThisObj;
-}
-
-function mergeNestedArray(nestedArray) {
-    var mergedObj = {};
-
-    for (var x = 0; x < nestedArray.length; x++) {
-        if (Array.isArray(nestedArray[x])) {
-            for (var i = 0; i < nestedArray[x].length; i++) {
-                if (Array.isArray(nestedArray[x][i])) {
-                    for (var y = 0; y < nestedArray[x][i].length; y++) {
-                        extend(true, mergedObj, nestedArray[x][i][y]);
-                    }
-                } else { extend(true, mergedObj, nestedArray[x][i]); }
-            }
-        } else { extend(true, mergedObj, nestedArray[x]); }
-    }
-
-    return mergedObj;
 }
